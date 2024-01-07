803d4bbc1be49a437adec10654d4e59728fa5ad3
Mark Andrachek <mandrachek@marksmacstudio.lan>
2024-01-07 16:51:42 -0500

    Add home screen layout

--- app/src/main/java/com/andrachek/singleLine/HomeFragment.kt
+++ app/src/main/java/com/andrachek/singleLine/HomeFragment.kt
@@ -1,6 +1,26 @@
 package com.andrachek.singleLine
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.andrachek.singleLine.databinding.HomeBinding
 
 class HomeFragment : Fragment() {
+    private var _binding: HomeBinding? = null
+    private val binding get() = _binding!!
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = HomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
